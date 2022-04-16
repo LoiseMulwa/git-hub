6de59808659c1580bdc0fcfe02af4c58317b9102
LoiseMulwa <loisemulwa7@gmail.com>
2022-04-16 23:39:25 +0300

    ft:Generated repos component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ReposComponent } from './repos/repos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    NavBarComponent
+    NavBarComponent,
+    ReposComponent
   ],
   imports: [
     BrowserModule,
