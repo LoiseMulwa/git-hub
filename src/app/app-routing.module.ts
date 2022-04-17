2018fcced183952ab80c28f03eadd2398e7f9052
LoiseMulwa <loisemulwa7@gmail.com>
2022-04-18 00:06:15 +0300

    ft: Imported components and added paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { ReposComponent } from './repos/repos.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent},
+  { path: 'repos', component:ReposComponent},
+  { path: 'nav-bar', component:NavBarComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
