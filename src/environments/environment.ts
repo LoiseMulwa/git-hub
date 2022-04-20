65b78ff6ffd80b6c11f6534067926f2fd1ce18a4
LoiseMulwa <loisemulwa7@gmail.com>
2022-04-20 09:45:38 +0300

    ft: Imported the service profile to the profile component

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:'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'
 };
 
 /*
