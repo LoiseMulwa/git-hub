57d95f1e74e08a0c522574a17e337d95acfd0280
LoiseMulwa <loisemulwa7@gmail.com>
2022-04-20 14:22:16 +0300

    fx:Added api link in the environment production

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -10,37 +10,37 @@ import { ProfileService } from '../profile.service';
   providers: [ProfileService]
 })
 export class FormComponent implements OnInit {
-  profile:any=[];
-  info:any=[];
-  repos:any=[];
-  username:any=[];
-  display:any=true;
+  profile: any = [];
+  info: any = [];
+  repos: any = [];
+  username: any = [];
+  display: any = true;
 
-  constructor (private profileService:ProfileService) { 
-    this.profileService.getUserRepos().subscribe(repos =>{
-      this.repos=repos
+  constructor(private profileService: ProfileService) {
+    this.profileService.getUserRepos().subscribe(repos => {
+      this.repos = repos
 
-  });
-  this.profileService.getUserInfo().subscribe(info =>{
-    this.info=info
-  });
+    });
+    this.profileService.getUserInfo().subscribe(info => {
+      this.info = info
+    });
   }
 
-  searchUser(){
-    this.display=false;
+  searchUser() {
+    this.display = false;
     this.profileService.updateProfile(this.username);
-    this.profileService.getProfileInfo().subscribe((profile)=>{
-    this.profile=profile
+    this.profileService.getProfileInfo().subscribe((profile) => {
+      this.profile = profile
     });
-    this.profileService.getRepos(this.username).subscribe((repos)=>{
-      this.repos=repos
+    this.profileService.getRepos(this.username).subscribe((repos) => {
+      this.repos = repos
     });
-     this.profileService.getProfileInfo().subscribe((info)=>{
-      this.info=info
+    this.profileService.getProfileInfo().subscribe((info) => {
+      this.info = info
       console.log(this.info);
     });
 
-}
+  }
   ngOnInit(): void {
   }
 
--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -10,36 +10,39 @@ export class ProfileService {
   private username: string;
   private _Url = 'https://api.github.com/users';
 
-  constructor(private http:HttpClient ) { this.username="LoiseMulwa"; }
-  ngOninit() { 
+  constructor(private http: HttpClient) { this.username = "LoiseMulwa"; }
+  ngOninit() {
     this.http.get<any>('https://api.github.com/users').subscribe((data) => {
       this.username = data.total;
       console.log(this.username);
     });
 
-}
-getUserInfo() {
-  return this.http.get('https://api.github.com/users/' + this.username); 
-}
-getUserRepos() {
-  return this.http.get(
-    'https://api.github.com/users/' + this.username + '/repos'
-  );  }
+  }
+  getUserInfo() {
+    return this.http.get('https://api.github.com/users/' + this.username);
+  }
+  getUserRepos() {
+    return this.http.get(
+      'https://api.github.com/users/' + this.username + '/repos'
+    );
+  }
 
   getProfiles() {
-    return this.http.get<any[]>(this._Url);  }
-     getProfileInfo(){  
-    return this.http.get('https://api.github.com/users/'+this.username+'?acess_token='+environment.apiKey);    }   
-
-    getRepos(user: any){
-      let userrepo = 
-      this.http.get('https://api.github.com/users/'+this.username+'/repos?acess_token='+environment.apiKey); 
-      return userrepo    
-    }
-    updateProfile(username:string){
-      this.username = username;
-    }
+    return this.http.get<any[]>(this._Url);
   }
+  getProfileInfo() {
+    return this.http.get('https://api.github.com/users/' + this.username + '?acess_token=' + environment.apiKey);
+  }
+
+  getRepos(user: any) {
+    let userrepo =
+      this.http.get('https://api.github.com/users/' + this.username + '/repos?acess_token=' + environment.apiKey);
+    return userrepo
+  }
+  updateProfile(username: string) {
+    this.username = username;
+  }
+}
 
 
 
